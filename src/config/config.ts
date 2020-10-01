7b27751952b057bd6cdf780b8bb57c3930bb1ba0
Alex Devero <deveroalex@gmail.com>
2020-10-01 09:03:13 +0200

    Add config for components

---
+++ src/config/config.ts
@@ -0,0 +1,178 @@
+export const navConfig = {
+  useDarkMode: true,
+  navLinks: [
+    {
+      navLinkTitle: '',
+      navLinkUrl: '',
+      navLinkOpenInNewWindow: true
+    },
+    {
+      navLinkTitle: '',
+      navLinkUrl: '',
+      navLinkOpenInNewWindow: true
+    }
+  ]
+}
+
+export const heroConfig = {
+  heroHeading: 'Hello, world!',
+  heroText: 'This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.',
+  heroHasCta: false,
+  heroCtaText: '',
+  heroCtaLink: '',
+  heroInputPlacehodler: 'Enter email...',
+  heroButtonText: 'Request access',
+}
+
+export const splitScreenConfig = {
+  splitScreens: [
+    {
+      splitScreenImageUrl: '',
+      splitScreenImageTitle: '',
+      splitScreenHeading: 'One',
+      splitScreenText: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo tenetur eligendi commodi qui culpa blanditiis.',
+      splitScreenDirection: 'image-left'
+    },
+    {
+      splitScreenImageUrl: '',
+      splitScreenImageTitle: '',
+      splitScreenHeading: 'Two',
+      splitScreenText: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo tenetur eligendi commodi qui culpa blanditiis.',
+      splitScreenDirection: 'image-right'
+    },
+    {
+      splitScreenImageUrl: '',
+      splitScreenImageTitle: '',
+      splitScreenHeading: 'Three',
+      splitScreenText: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo tenetur eligendi commodi qui culpa blanditiis.',
+      splitScreenDirection: 'image-left'
+    }
+  ]
+}
+
+export const featuresConfig = {
+  featuresNumOfRows: 2,
+  featuresItemsPerRow: 3,
+  features: [
+    {
+      featureImageUrl: '',
+      featureHeading: 'One',
+      featureText: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo tenetur eligendi commodi qui culpa blanditiis.'
+    },
+    {
+      featureImageUrl: '',
+      featureHeading: 'Two',
+      featureText: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo tenetur eligendi commodi qui culpa blanditiis.'
+    },
+    {
+      featureImageUrl: '',
+      featureHeading: 'Three',
+      featureText: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo tenetur eligendi commodi qui culpa blanditiis.'
+    },
+    {
+      featureImageUrl: '',
+      featureHeading: 'Four',
+      featureText: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo tenetur eligendi commodi qui culpa blanditiis.'
+    },
+    {
+      featureImageUrl: '',
+      featureHeading: 'Five',
+      featureText: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo tenetur eligendi commodi qui culpa blanditiis.'
+    },
+    {
+      featureImageUrl: '',
+      featureHeading: 'Six',
+      featureText: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo tenetur eligendi commodi qui culpa blanditiis.'
+    }
+  ]
+}
+
+export const pricingConfig = {
+  pricingCurrency: '$',
+  pricingType: 'monthly',
+  lowTier: {
+    tierHeading: 'Starter',
+    tierText: 'With supporting text below as a natural lead-in to additional content.',
+    tierFetures: {
+      tierFetureOne: 'One',
+      tierFetureTwo: 'Two',
+      tierFetureThree: 'Three'
+    },
+    tierPrice: '29'
+  },
+  mediumTier: {
+    tierHeading: 'Standard',
+    tierText: 'With supporting text below as a natural lead-in to additional content.',
+    tierFetures: {
+      tierFetureOne: 'One',
+      tierFetureTwo: 'Two',
+      tierFetureThree: 'Three',
+      tierFetureFour: 'Four',
+      tierFetureFive: 'Five'
+    },
+    tierPrice: '49'
+  },
+  topTier: {
+    tierHeading: 'Pro',
+    tierText: 'With supporting text below as a natural lead-in to additional content.',
+    tierFetures: {
+      tierFetureOne: 'One',
+      tierFetureTwo: 'Two',
+      tierFetureThree: 'Three',
+      tierFetureFour: 'Four',
+      tierFetureFive: 'Five',
+      tierFetureSix: 'Six'
+    },
+    tierPrice: '99'
+  }
+}
+
+export const signUpConfig = {
+  signUpHeading: 'Try "X"',
+  signUpText: 'Sign up below to get started with "X".',
+  signUpInputPlacehodler: 'Enter your email...',
+  signUpButtonText: 'Sign me up',
+  signUpErrorMessage: 'Please use correct email address.'
+}
+
+export const shareConfig = {
+  shareText: 'Please, help us spread the word about "X" by sharing it with your friends.',
+  shareLinks: [
+    {
+      shareLinkIconName: 'fab fa-twitter',
+      shareLinkTitle: 'Twitter',
+      shareLinkUrl: ''
+    },
+    {
+      shareLinkIconName: 'fab fa-facebook-square',
+      shareLinkTitle: 'Facebook',
+      shareLinkUrl: ''
+    },
+    {
+      shareLinkIconName: 'fab fa-reddit',
+      shareLinkTitle: 'Reddit',
+      shareLinkUrl: ''
+    },
+    {
+      shareLinkIconName: 'far fa-envelope',
+      shareLinkTitle: 'Email',
+      shareLinkUrl: ''
+    }
+  ]
+}
+
+export const footerConfig = {
+  footerCopyrightText: `Copyright ${new Date().getFullYear()}. Alex Devero, all rights reserved.`,
+  footerLinks: [
+    {
+      footerLinkIconName: 'far fa-envelope',
+      footerLinkTitle: 'Email',
+      footerLinkUrl: ''
+    },
+    {
+      footerLinkIconName: 'fab fa-product-hunt',
+      footerLinkTitle: 'Product Hunt',
+      footerLinkUrl: ''
+    }
+  ]
+}
