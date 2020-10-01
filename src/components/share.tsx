9ae0c8de5cb7844576ec9fe11893ad80dcc9dbe6
Alex Devero <deveroalex@gmail.com>
2020-10-01 09:02:06 +0200

    Add share component

---
+++ src/components/share.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import { shareConfig } from './../config/config'
+
+export const Share = () => (
+  <>
+    <p className="mb-4 text-center">{shareConfig.shareText}</p>
+
+    {shareConfig.shareLinks.length > 0 && (
+      <ul className="list-inline d-flex justify-content-center align-items-center m-0">
+        {shareConfig.shareLinks.map(shareLink => {
+          return (
+            <li>
+              <a
+                href={shareLink.shareLinkUrl}
+                className={`mr-3 h3 text-decoration-none`}
+                title={shareLink.shareLinkTitle}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span className={`${shareLink.shareLinkIconName}`} />
+              </a>
+            </li>
+          )
+        })}
+      </ul>
+    )}
+  </>
+)
