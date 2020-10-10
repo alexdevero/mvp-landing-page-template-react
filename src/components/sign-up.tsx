3fd4f1b84ee951f9333285e45278ec8442113461
Alex Devero <deveroalex@gmail.com>
2020-10-10 15:13:49 +0200

    Redesign sign up component

--- src/components/sign-up.tsx
+++ src/components/sign-up.tsx
@@ -11,44 +11,62 @@ interface SignUpUI {
 }
 
 export const SignUp = (props: SignUpUI) => (
-  <>
-    <h1 className="mt-0 mb-4 h2 text-center">{signUpConfig.signUpHeading}</h1>
+  <div className="row justify-content-center position-relative align-items-center" style={{ minHeight: '440px' }}>
+    <img
+      className="position-absolute"
+      style={{
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: '800px',
+        maxWidth: '100%',
+        objectFit: 'contain'
+      }}
+      src={require('./../images/sing-up-bcg.svg')}
+      alt=""
+    />
 
-    <p className="m-0 text-center">{signUpConfig.signUpButtonText}</p>
+    <div className="col-md-10 col-lg-8 col-xl-7">
+      <div className="card shadow border-0">
+        <div className="card-body p-5">
+          <h1 className="mt-0 mb-4 h3 text-center">{signUpConfig.signUpHeading}</h1>
 
-    <div className="row justify-content-center mt-4">
-      <div className="col-sm-9 col-md-8 col-lg-6 col-xl-5">
-        <div className="form-inline">
-          <div className="input-group flex-grow-1">
-            {/* <div className="input-group-prepend">
-              <div className="input-group-text">@</div>
-            </div> */}
+          <p className="m-0 lead font-weight-normal text-center">{signUpConfig.signUpText}</p>
 
-            <input
-              type="text"
-              className="form-control"
-              id="inlineFormInputGroup"
-              placeholder={signUpConfig.signUpInputPlacehodler}
-              value={props.signUpEmail}
-              onChange={(event) => props.handleSetSignUpEmail(event.currentTarget.value)}
-            />
+          <div className="form-inline mt-4 form-row">
+            <div className="col-md-8">
+              {/* <div className="input-group-prepend">
+                <div className="input-group-text">@</div>
+              </div> */}
+
+              <input
+                type="text"
+                className="form-control w-100"
+                id="inlineFormInputGroup"
+                placeholder={signUpConfig.signUpInputPlaceholder}
+                value={props.signUpEmail}
+                onChange={(event) => props.handleSetSignUpEmail(event.currentTarget.value)}
+              />
+            </div>
+
+            <div className="col-md-4">
+              <button
+                type="button"
+                className="btn btn-primary w-100 mt-3 mt-md-0"
+                onClick={props.handleSubmitForm}
+              >{signUpConfig.signUpButtonText}</button>
+            </div>
           </div>
 
-          <button
-            type="button"
-            className="btn btn-primary flex-grow-1 flex-md-grow-0 mt-3 mt-md-0"
-            onClick={props.handleSubmitForm}
-          >{signUpConfig.signUpButtonText}</button>
+          {props.successMessage.length > 0 && (
+            <div className="text-danger font-weight-bold small mt-2">{props.errorMessage}</div>
+          )}
+
+          {props.errorMessage.length > 0 && (
+            <div className="text-danger font-weight-bold small mt-2">{props.errorMessage}</div>
+          )}
         </div>
-
-        {props.successMessage.length > 0 && (
-          <div className="text-danger font-weight-bold small mt-2">{props.errorMessage}</div>
-        )}
-
-        {props.errorMessage.length > 0 && (
-          <div className="text-danger font-weight-bold small mt-2">{props.errorMessage}</div>
-        )}
       </div>
     </div>
-  </>
+  </div>
 )
