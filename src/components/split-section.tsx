22963342bfa0de2cf290df509357536c16e351d4
Alex Devero <deveroalex@gmail.com>
2021-02-10 11:01:48 +0100

    Update SplitSection images and text

--- src/components/split-section.tsx
+++ src/components/split-section.tsx
@@ -13,12 +13,12 @@ export const SplitSection = (props: SplitSectionUI) => (
     <div className="col-md-6">
       <h2 className="mt-4 mt-md-0 mb-3 h3">{props.heading}</h2>
 
-      <p className="lead font-weight-normal">{props.textContent}</p>
+      <p className="lead font-weight-normal text-secondary">{props.textContent}</p>
     </div>
 
     <div className={`col-md-6 d-flex ${props.direction === 'image-right' ? 'justify-content-md-end' : 'justify-content-md-start'}`}>
       {props.imgUrl.length === 0 ? (
-        <svg className="bd-placeholder-img rounded float-left" width="200" height="200" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 200x200"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="35%" y="50%" fill="#dee2e6" dy=".3em">200x200</text></svg>
+        <svg className="bd-placeholder-img rounded float-left" width="300" height="300" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 300x300"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="40%" y="50%" fill="#dee2e6" dy=".3em">200x200</text></svg>
       ) : (
         <img src={props.imgUrl} alt={props.imgTitle} />
       )}
