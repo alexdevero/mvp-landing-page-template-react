be421b83cded501b06ff6619bfcac6f0ffa4202e
Alex Devero <deveroalex@gmail.com>
2021-02-10 11:01:29 +0100

    Update features typography

--- src/components/features.tsx
+++ src/components/features.tsx
@@ -16,9 +16,9 @@ export const Features = () => (
             <svg className="bd-placeholder-img rounded mb-4" width="100" height="100" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 100x100"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="20%" y="50%" fill="#dee2e6" dy=".3em">100x100</text></svg>
           )}
 
-          <h2 className="mt-0 mb-3 h4">{feature.featureHeading}</h2>
+          <h2 className="mt-0 mb-3 h5">{feature.featureHeading}</h2>
 
-          <p className="m-0 font-weight-normal">{feature.featureText}</p>
+          <p className="m-0 font-weight-normal text-secondary">{feature.featureText}</p>
         </div>
       )
     })}
