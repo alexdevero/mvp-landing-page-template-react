c9638068f9c6747e07b08296ce8d43d5ef00b8db
Alex Devero <deveroalex@gmail.com>
2021-02-18 14:08:08 +0100

    Add id prop

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -5,10 +5,14 @@ interface ContainerUI {
   hasContainer: boolean;
   children: JSX.Element;
   className?: string;
+  id?: string;
 }
 
 export const Container = (props: ContainerUI) => (
-  <section className={`section section-${props.sectionName} pt-7 pb-7${props.className ? ' ' + props.className : ''}`}>
+  <section
+    className={`section section-${props.sectionName} pt-7 pb-7${props.className ? ' ' + props.className : ''}`}
+    id={`${props.id ? props.id : ''}`}
+  >
     {props.hasContainer ? (
       <div className="container">
         {props.children}
